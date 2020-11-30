2b320b709815914a07c296f8f2231d67d90dac97
iColtz <max.hurst@hotmail.com>
2020-11-30 22:30:04 +0000

    Client(message listener): handling handled commands

--- src/bot/client/Client.ts
+++ src/bot/client/Client.ts
@@ -1,8 +1,12 @@
-import { Client, Message } from 'discord.js';
+import { Client, Collection, Message } from 'discord.js';
+import { CommandRegistry } from '../struct/registries/export/RegistryIndex';
+import { CommandOptions } from '../types/Options';
 import 'dotenv/config';
 
 class Bot extends Client {
-  public prefix: string | undefined;
+  public prefix: string;
+
+  public commands: Collection<string, object>;
 
   public constructor() {
     super({
@@ -10,21 +14,35 @@ class Bot extends Client {
       disableMentions: 'everyone',
     });
 
-    this.prefix = process.env.prefix;
+    this.prefix = process.env.prefix ?? '!';
+
+    this.commands = new Collection();
 
     this.on('ready', () => {
       console.log('Ready!');
     });
 
     this.on('message', (message: Message) => {
-      if (message.content === this.prefix + 'ping') {
-        message.reply('Pong!')
+      if (!message.content.startsWith(this.prefix) || message.author.bot) return;
+      const args: string[] = message.content.split(/ +/);
+      const commandName: string = args[0].slice(this.prefix.length);
+      const command: any = this.commands.get(commandName);
+      if (command) {
+        try {
+          command.exec(message, args);
+        }
+        catch (error) {
+          console.log(error);
+          message.reply('there was an error running this command.')
+        }
       }
     });
   }
 
   public start() {
     super.login(process.env.BOT_TOKEN);
+    CommandRegistry(this);
+    console.log(this.commands);
   }
 }
 
