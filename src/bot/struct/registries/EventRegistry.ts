071950537ad4dc42c4e8f62afd599183273e176c
iColtz <max.hurst@hotmail.com>
2020-12-01 17:42:16 +0000

    registries(Event): refactor

--- src/bot/struct/registries/EventRegistry.ts
+++ src/bot/struct/registries/EventRegistry.ts
@@ -6,8 +6,7 @@ import { resolve } from 'path';
 const registerEvents: Function = (client: Bot) => {
   const eventFiles = sync(resolve('src/bot/events/**/*'));
   eventFiles.forEach(file => {
-    const File = require(file).default;
-    const event: Event = new File();
+    const event: Event = new (require(file).default);
     event.client = client;
     client.events.set(event.name, event);
     client.on(event.name, (...args) => event.exec(...args));
