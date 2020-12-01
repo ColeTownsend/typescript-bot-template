445ad0f1a1f5d0d999372002c4f863d40270aaea
iColtz <max.hurst@hotmail.com>
2020-12-01 18:39:08 +0000

    events(message): finding command by name or alias

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -1,6 +1,7 @@
 import Event from '../struct/Event';
 import { Message } from 'discord.js';
 import 'dotenv/config';
+import Command from '../struct/Command';
 
 abstract class MessageEvent extends Event {
   constructor() {
@@ -13,7 +14,8 @@ abstract class MessageEvent extends Event {
     if (!message.content.startsWith(this.client.prefix) || message.author.bot) return;
     const args: string[] = message.content.split(/ +/);
     const commandName: string = args[0].slice(this.client.prefix.length);
-    const command: any = this.client.commands.get(commandName);
+    const command: any = this.client.commands.get(commandName) 
+      ?? this.client.commands.find((cmd: any) => cmd.aliases && cmd.aliases.includes(commandName));
     if (command) {
       if (command.ownerOnly && message.author.id !== process.env.BOT_OWNER_ID) {
         return message.channel.send('This command can only be used by the owner of the bot.');
