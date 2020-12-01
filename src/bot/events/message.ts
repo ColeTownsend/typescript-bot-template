536a2f543b62d960c65d88bb377e9fa01ce4864b
iColtz <max.hurst@hotmail.com>
2020-12-01 19:23:48 +0000

    events(message): returning if the member dosn't have correct permissions

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -1,5 +1,5 @@
 import Event from '../struct/Event';
-import { Message } from 'discord.js';
+import { Message, PermissionString, TextChannel } from 'discord.js';
 import 'dotenv/config';
 
 abstract class MessageEvent extends Event {
@@ -18,6 +18,21 @@ abstract class MessageEvent extends Event {
     if (command) {
       if (command.ownerOnly && message.author.id !== process.env.BOT_OWNER_ID) {
         return message.channel.send('This command can only be used by the owner of the bot.');
+      }
+      if (message.channel instanceof TextChannel) {
+        const userPermissions: PermissionString[] = command.userPermissions;
+        const missingPermissions: Array<PermissionString> = new Array;
+        if (userPermissions.length) {
+          for (let i = 0; i < userPermissions.length; i++) {
+            const hasPermission = message.member?.hasPermission(userPermissions[i]);
+            if (!hasPermission) {
+              missingPermissions.push(userPermissions[i]);
+            }
+          }
+          if (missingPermissions.length) {
+            return message.channel.send(`Your missing these required permissions: ${missingPermissions.join(', ')}`);
+          }
+        }
       }
       try {
         command.exec(message, args);
