d6417711c4995ac2dda0e3b38000c15e7805d2b6
iColtz <max.hurst@hotmail.com>
2020-12-02 18:33:47 +0000

    classes(Command): refactor

--- src/bot/struct/Command.ts
+++ src/bot/struct/Command.ts
@@ -4,42 +4,25 @@ import Bot from '../client/Client';
 
 abstract class Command {
   public name: string;
-
   public aliases: string[];
-
   public description: string;
-
   public category: string;
-
   public ownerOnly: boolean;
-
   public guildOnly: boolean;
-
   public requiredArgs: number;
-
   public userPermissions: PermissionString[];
-
   public clientPermissions: PermissionString[];
-
   public abstract client: Bot;
 
   constructor(options: CommandType) {
     this.name = options.name;
-
     this.aliases = options.aliases ?? [];
-
     this.description = options.description;
-
     this.category = options.category ?? 'Misc';
-
     this.ownerOnly = options.ownerOnly ?? false;
-
     this.guildOnly = options.guildOnly ?? false;
-
     this.requiredArgs = options.requiredArgs ?? 0;
-
     this.userPermissions = options.userPermissions ?? [];
-
     this.clientPermissions = options.clientPermissions ?? [];
   }
 
