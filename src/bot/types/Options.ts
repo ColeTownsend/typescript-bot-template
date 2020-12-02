d1bda165d1368b982b245ebff19bbf77755a9f60
iColtz <max.hurst@hotmail.com>
2020-12-02 22:04:44 +0000

    types(CommandOptions): added usage

--- src/bot/types/Options.ts
+++ src/bot/types/Options.ts
@@ -4,6 +4,7 @@ export interface CommandOptions {
   name: string,
   aliases?: string[],
   description: string,
+  usage?: string,
   category?: string,
   cooldown?: number,
   ownerOnly?: boolean,
