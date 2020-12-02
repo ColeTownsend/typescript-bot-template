a8fcc38f6e6b1e53b3c6fec7816772eacf38ebc2
iColtz <max.hurst@hotmail.com>
2020-12-02 17:14:43 +0000

    registries(Event): if event is once emitting only once

--- src/bot/struct/registries/EventRegistry.ts
+++ src/bot/struct/registries/EventRegistry.ts
@@ -7,9 +7,10 @@ const registerEvents: Function = (client: Bot) => {
   const eventFiles = sync(resolve('src/bot/events/**/*'));
   eventFiles.forEach(file => {
     const event: Event = new (require(file).default);
+    console.log(event);
     event.client = client;
     client.events.set(event.name, event);
-    client.on(event.name, (...args: any[]) => event.exec(...args));
+    client[event.type ? 'once' : 'on'](event.name, (...args: any[]) => event.exec(...args));
   });
 }
 
